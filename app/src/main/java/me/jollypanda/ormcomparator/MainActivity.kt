909933cd25937bd8868421c4a33d89b712b7e0b5
Yamushev Igor <yamushev.igro@yandex.ru>
2016-10-11 18:45:33 +0300

    Change logic for average calculation

--- app/src/main/java/me/jollypanda/ormcomparator/MainActivity.kt
+++ app/src/main/java/me/jollypanda/ormcomparator/MainActivity.kt
@@ -26,6 +26,11 @@ import me.jollypanda.ormcomparator.utils.ORM_COUNT
  */
 class MainActivity : BaseActivity(), MainContract.View {
 
+    /** 0 element is a counter for write test attempts */
+    var mWriteSumList = FloatArray(ORM_COUNT + 1, { i -> 0f})
+    /** 0 element is a counter for read test attempts */
+    var mReadSumList = FloatArray(ORM_COUNT + 1, {i -> 0f})
+
     enum class Action {
         READ, WRITE
     }
@@ -57,17 +62,23 @@ class MainActivity : BaseActivity(), MainContract.View {
             tvRes.tag = result.workTimeMills
             tvRes.layoutParams = ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT)
-            if (result.readOrWrite == 1)
+            if (result.readOrWrite == 1) {
                 llWriteInfoContainer.addView(tvRes)
-            else
+                mWriteSumList[i + 1] = mWriteSumList[i + 1] + result.workTimeMills
+            } else {
                 llReadInfoContainer.addView(tvRes)
+                mReadSumList[i + 1] = mReadSumList[i + 1] + result.workTimeMills
+            }
 
             if (i == ORM_COUNT - 1) {
                 if (result.readOrWrite == 1) {
                     btnStartReadTest.isEnabled = true
                     pbMainWrite.visibility = View.GONE
-                } else
+                    mWriteSumList[0] = mWriteSumList[0] + 1
+                } else {
                     pbMainRead.visibility = View.GONE
+                    mReadSumList[0] = mReadSumList[0] + 1
+                }
                 val divider = View(this)
                 divider.setBackgroundColor(Color.GRAY)
                 divider.setPadding(0, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
@@ -82,44 +93,36 @@ class MainActivity : BaseActivity(), MainContract.View {
                         ).toInt())
                 if (result.readOrWrite == 1) {
                     llWriteInfoContainer.addView(divider)
-                    calculateAverage(llWriteInfoContainer)
+                    calculateAverage(Action.WRITE)
                 } else {
                     llReadInfoContainer.addView(divider)
-                    calculateAverage(llReadInfoContainer)
+                    calculateAverage(Action.READ)
                 }
             }
             invalidateChart(i, result)
         }
     }
 
-    private fun calculateAverage(container: LinearLayout) {
-        var list: MutableList<Float> = mutableListOf()
-        for (i in 0..ORM_COUNT - 1)
-            list.add(i, 0f)
-        var counter: Float = 0f
-        for (i in 0..container.childCount - 1) {
-            if (container.getChildAt(i) is TextView) {
-                val child = container.getChildAt(i)
-                val pos = i.mod(ORM_COUNT + 1)
-                list[pos] = list[pos] + child.tag.toString().toFloat()
-            } else
-                counter += 1
+    private fun calculateAverage(action: Action) {
+        when (action) {
+            Action.WRITE -> {
+                var list = mWriteSumList.clone()
+                for (i in 1..list.size - 1)
+                    list[i] = list[i] / list[0]
+                addAverageToContainer(list, llWriteAverageContainer)
+            }
+            Action.READ -> {
+                var list = mReadSumList.clone()
+                for (i in 1..list.size - 1)
+                    list[i] = list[i] / list[0]
+                addAverageToContainer(list, llReadAverageContainer)
+            }
         }
-        val size = list.size
-        for (i in 0..size - 1) {
-            list[i] = list[i] / counter
-        }
-
-        if (container.id == R.id.llWriteInfoContainer)
-            addAverageToContainer(list, llWriteAverageContainer)
-        else
-            addAverageToContainer(list, llReadAverageContainer)
-
     }
 
-    private fun addAverageToContainer(list: MutableList<Float>, container: LinearLayout) {
+    private fun addAverageToContainer(list: FloatArray, container: LinearLayout) {
         container.removeAllViews()
-        for (i in 0..list.size - 1) {
+        for (i in 1..list.size - 1) {
             val tv = TextView(this)
             tv.text = list[i].toString()
             tv.layoutParams = ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
